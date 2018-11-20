1ec79e5bec9e1753a3aca2c637edbd618ab9dcc2
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-20 15:07:57 +0100

    Implemented spring wsd

---
+++ src/main/java/rulebase/RuleBaseApplication.java
@@ -0,0 +1,11 @@
+package rulebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuleBaseApplication.class, args);
+    }
+}
