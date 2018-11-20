88c140da2acc120209cb2542e9a15af8a1dc96f4
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-11-20 11:06:19 +0100

    rule base final

---
+++ src/main/java/rulebase/Bank.java
@@ -0,0 +1,33 @@
+
+package rulebase;
+
+/**
+ *
+ * @author Cherry Rose Semeña
+ */
+public class Bank {
+    private String name;
+    private String routingKey;
+
+    public Bank(String name, String routingKey) {
+        this.name = name;
+        this.routingKey = routingKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+    
+}
